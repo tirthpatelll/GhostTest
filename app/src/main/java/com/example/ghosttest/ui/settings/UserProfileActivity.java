92982a43932f51403470eafd34866502074ad7f7
tirthpatelll <158233171+tirthpatelll@users.noreply.github.com>
2025-02-22 15:04:03 -0500

    improved UserProfileActivity
    added lambda functions

--- app/src/main/java/com/example/ghosttest/ui/settings/UserProfileActivity.java
+++ app/src/main/java/com/example/ghosttest/ui/settings/UserProfileActivity.java
@@ -9,9 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.ghosttest.R;
 import com.example.ghosttest.utils.FireStoreManager;
 import com.example.ghosttest.utils.FirebaseAuthManager;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentSnapshot;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -30,35 +27,31 @@ public class UserProfileActivity extends AppCompatActivity {
         tvUsername = findViewById(R.id.username_value);
         tvLevel = findViewById(R.id.in_game_level_value);
         tvXp = findViewById(R.id.total_xp_value);
+        loadUserData();
     }
 
     private void loadUserData() {
         // Fetch user data from Firestore
         String userId = authManager.getUserID();
         firestoreManager.getUserData(userId,
-                new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            String username = documentSnapshot.getString("username");
+                documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        String username = documentSnapshot.getString("username");
+                        try {
                             long level = documentSnapshot.getLong("ingameLevel");
                             long xp = documentSnapshot.getLong("totalXp");
-
                             // Update UI
                             tvUsername.setText(username);
                             tvLevel.setText(String.valueOf(level));
-                            tvXp.setText(String.valueOf(xp) + " XP");
-                        } else {
-                            Log.d("Firestore", "No such document");
+                            tvXp.setText((int) xp);
+                        } catch (NullPointerException e) {
+                            Log.e("Firestore", "Error fetching user data", e);
                         }
+                    } else {
+                        Log.d("Firestore", "No such document");
                     }
                 },
-                new OnFailureListener() {
-                    @Override
-                    public void onFailure(Exception e) {
-                        Log.e("Firestore", "Error fetching user data", e);
-                    }
-                }
+                e -> Log.e("Firestore", "Error fetching user data", e)
         );
     }
 
